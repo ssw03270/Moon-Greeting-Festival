1a45e44e9e216eeec3e70221bd2e447c52c9b4b3
SeungWon Seo <ttd8591@gmail.com>
2022-12-17 19:36:26 +0900

    add audio

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioPlayer : MonoBehaviour
+{
+    public AudioClip audio;
+
+    private AudioSource audioSource;
+    private bool firstPlay = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (audio != null)
+        {
+            audioSource.clip = audio;
+            
+            if(other.tag == "Player" && firstPlay)
+            {
+                audioSource.Stop();
+                audioSource.time = 0;
+                audioSource.Play();
+                firstPlay = false;
+            }
+        }
+    }
+}
